e94c64a8ad0b58c10390cdbb6305304fa6d54b78
Sohag Hasan <shohaghasan575@gmail.com>
2022-01-19 17:07:43 +0600

    init | simple todo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('');
+
+  function addTodo() {
+    setTodos([...todos, todo]);
+    setTodo('');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Top Secret ToDO
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div>
+          <input type="text" value={todo} onChange={e => setTodo(e.target.value)} className="input" placeholder="What you want to do?" />
+          <button className="btn" onClick={addTodo}>Add</button>
+
+          <ul>
+            {todos.map((todo, index) => (
+              <li key={index}>
+                <label>
+                  <input type="checkbox" />
+                  {todo}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
